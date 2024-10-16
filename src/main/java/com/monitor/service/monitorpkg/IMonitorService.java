3c9fa14320861fd51c887a6fc2973bfe714d2a94
gi1tee <gouzhonglin@huawei.com>
2024-10-16 10:55:16 +0800

    init

--- src/main/java/com/monitor/service/monitorpkg/IMonitorService.java
+++
@@ -1,9 +0,0 @@
-package com.monitor.service.monitorpkg;
-
-public interface IMonitorService {
-    /**
-     * update upstream.
-     * @param requestBody request body.
-     */
-    void updateUpstream(String requestBody);
-}
